8da6cdf2038326599ed64fdd331254c1e44f1fb4
pul <paogravoso@gmail.com>
2020-01-31 17:58:42 +0800

    working AboutHabPage, 'lorem ipsum content'

--- frontend/src/HabAdvisoryPage.react.js
+++ frontend/src/HabAdvisoryPage.react.js
@@ -18,68 +18,68 @@ function HabAdvisoryPage(): React.Node {
   return (
     <SiteWrapper>
       <Page.Content title="Harmful Algal Bloom Advisory">
-      <Grid.Row>
-        <Grid.Col lg={8}>
-          <Container>
-            <Card>
-              <Card.Header>
-                <Card.Title>PDF Viewer</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
-                fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
-                velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
-                temporibus!
-              </Card.Body>
-              <Card.Footer>
-                <a href="https://www.bfar.da.gov.ph/index.jsp" target="_blank">View in BFAR Website </a>
-              </Card.Footer>
-            </Card>
-          </Container>
-        </Grid.Col>
-        <Grid.Col lg={4}>
-        <Card>
-          <Card.Header>
-            <Card.Title>View Previous Advisories</Card.Title>
-          </Card.Header>
-          <Card.Body>
-            <Calendar
-            />
-          </Card.Body>
-          <Card.Footer>
+        <Grid.Row>
+          <Grid.Col lg={8}>
+            <Container>
+              <Card>
+                <Card.Header>
+                  <Card.Title>PDF Viewer</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti
+                  fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit
+                  velit vitae voluptatem. A consequuntur, deserunt eaque error nulla
+                  temporibus!
+                </Card.Body>
+                <Card.Footer>
+                  <a href="https://www.bfar.da.gov.ph/index.jsp" target="_blank">View in BFAR Website </a>
+                </Card.Footer>
+              </Card>
+            </Container>
+          </Grid.Col>
+          <Grid.Col lg={4}>
+          <Card>
+            <Card.Header>
+              <Card.Title>View Previous Advisories</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Calendar
+              />
+            </Card.Body>
+            <Card.Footer>
 
-          </Card.Footer>
-        </Card>
-        </Grid.Col>
-      </Grid.Row>
+            </Card.Footer>
+          </Card>
+          </Grid.Col>
+        </Grid.Row>
       </Page.Content>
     </SiteWrapper>
   );
--- frontend/src/HabPortal.react.js
+++ frontend/src/HabPortal.react.js
@@ -1,5 +1,3 @@
-// @flow
-
 import * as React from "react";
 import Leaflet from 'leaflet';
 
