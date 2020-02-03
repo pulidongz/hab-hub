85d473cf395afd231969deedecee2addb16b93cb
pul <paogravoso@gmail.com>
2020-02-03 16:20:21 +0800

    About page topic hyperlinks and css

--- frontend/src/AboutHabPage.react.js
+++ frontend/src/AboutHabPage.react.js
@@ -15,62 +15,68 @@ function AboutHabPage() {
   			<Grid.Row>
 	       		<Grid.Col lg={3}>
 		       		<div className="sticky-top" id="abouthab-cards">
-						<Card title="Harmful Algal Blooms" isCollapsed isCollapsible>
-					      <Card.Status color="orange"/>
-					      <Card.Body>
-					      	<table className="ml-5 col text-md-left">
-					        	<tr><a href="#subtopic1" >Subtopic 1</a></tr>
-					        	<tr><a href="#subtopic2" >Subtopic 2</a></tr>
-					        	<tr><a href="#subtopic3" >Subtopic 3</a></tr>
-					        </table>
-					      </Card.Body>
-					    </Card>
-					    <Card title="HAB Monitoring" isCollapsed isCollapsible>
-					      <Card.Status color="orange"/>
-					      <Card.Body>
-							<table className="ml-5 col text-md-left">
-						        <tr><a href="#site_locations" >Site Locations</a></tr>
-						        <tr><a href="#monitoring_activities" >Monitoring Activites</a></tr>
-						        <tr><a href="#monitoring_protocol" >Monitoring Protocol</a></tr>
-						        <tr><a href="#data_contri_flow" >Data Contribution Flow</a></tr>
-						        <tr><a href="#data_file_format" >Data File Format</a></tr> 
-							</table>
-					      </Card.Body>
-					    </Card>
-					    <Card title="About the Project" isCollapsed isCollapsible>
-					      <Card.Status color="orange"/>
-					      <Card.Body>
-					      	<table className="ml-5 col text-md-left">
-						        <tr><a href="#hab_project" >HAB Project</a></tr>
-						        <tr><a href="#about_the_team" >About the Team</a></tr>
-						        <tr><a href="#project_partners" >Project Partners</a></tr>
-						        <tr><a href="#contact_info" >Contact Information</a></tr>
-							</table>
-					      </Card.Body>
-					    </Card>
-					    <Card title="FAQ" isCollapsed isCollapsible>
-					      <Card.Status color="orange"/>
-					      <Card.Body>
-					      	<table className="ml-5 col text-md-left">
-						        <tr><a href="#browser_compatibility" >Browser Compatibility</a></tr>
-							</table> 
-					      </Card.Body>
-					    </Card>
-					    <Card title="Model Release Notes" isCollapsed isCollapsible>
-					      <Card.Status color="orange"/>
-					      <Card.Body>
-					      	<table className="ml-5 col text-md-left">
-						        <tr><a href="#further_ref" >Further Reading / References</a></tr>
-							</table> 
-					      </Card.Body>
-					    </Card>
-					</div>
+								<a className="nodecorate" href="#hab">
+									<Card title="Harmful Algal Blooms" statusColor="orange" isCollapsed isCollapsible>
+							      <Card.Body>
+							      	<table className="ml-5 col text-md-left">
+							        	<tr><a href="#subtopic1" >Subtopic 1</a></tr>
+							        	<tr><a href="#subtopic2" >Subtopic 2</a></tr>
+							        	<tr><a href="#subtopic3" >Subtopic 3</a></tr>
+							        </table>
+							      </Card.Body>
+							    </Card>
+								</a>
+								<a className="nodecorate" href="#hab_monitoring">
+							    <Card title="HAB Monitoring" statusColor="orange" isCollapsed isCollapsible>
+							      <Card.Body>
+											<table className="ml-5 col text-md-left">
+								        <tr><a href="#site_locations" >Site Locations</a></tr>
+								        <tr><a href="#monitoring_activities" >Monitoring Activites</a></tr>
+								        <tr><a href="#monitoring_protocol" >Monitoring Protocol</a></tr>
+								        <tr><a href="#data_contri_flow" >Data Contribution Flow</a></tr>
+								        <tr><a href="#data_file_format" >Data File Format</a></tr> 
+											</table>
+							      </Card.Body>
+							    </Card>
+							   </a>
+							  <a className="nodecorate" href="#about_hab_project">
+							    <Card title="About the Project" statusColor="orange" isCollapsed isCollapsible>
+							      <Card.Body>
+							      	<table className="ml-5 col text-md-left">
+								        <tr><a href="#hab_project" >HAB Project</a></tr>
+								        <tr><a href="#about_the_team" >About the Team</a></tr>
+								        <tr><a href="#project_partners" >Project Partners</a></tr>
+								        <tr><a href="#contact_info" >Contact Information</a></tr>
+											</table>
+							      </Card.Body>
+							    </Card>
+							  </a>
+							  <a className="nodecorate" href="#faq">
+							    <Card title="FAQ" statusColor="orange" isCollapsed isCollapsible>
+							      <Card.Body>
+							      	<table className="ml-5 col text-md-left">
+								        <tr><a href="#browser_compatibility" >Browser Compatibility</a></tr>
+											</table> 
+							      </Card.Body>
+							    </Card>
+							  </a>
+							  <a className="nodecorate" href="#model_release">
+							    <Card title="Model Release Notes" isCollapsed isCollapsible>
+							      <Card.Status color="orange"/>
+							      <Card.Body>
+							      	<table className="ml-5 col text-md-left">
+								        <tr><a href="#further_ref" >Further Reading / References</a></tr>
+											</table> 
+							      </Card.Body>
+							    </Card>
+							  </a>
+							</div>
 	       		</Grid.Col>
 	       		<Grid.Col lg={9}>
 	       			<Card>
 		       			<Card.Body>
 			            	<div id="subtopic1" className="container-fluid">
-				            	<h1>Harmful Algal Blooms</h1>
+				            	<h1 id="hab">Harmful Algal Blooms</h1>
 								<h2>Subtopic 1</h2>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus!</p>
 							</div>
@@ -84,7 +90,7 @@ function AboutHabPage() {
 							</div>
 
 							<div id="site_locations" className="container-fluid">
-								<h1>HAB Monitoring</h1>
+								<h1 id="hab_monitoring">HAB Monitoring</h1>
 							  	<h2>Site Locations</h2>
 							  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus!</p>
 							</div>
@@ -106,7 +112,7 @@ function AboutHabPage() {
 							</div>
 
 			            	<div id="hab_project" className="container-fluid">
-			            		<h1>About the Project</h1>
+			            		<h1 id="about_hab_project">About the Project</h1>
 							  	<h2>HAB Project</h2>
 							  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus!</p>
 							</div>
@@ -124,13 +130,13 @@ function AboutHabPage() {
 							</div>
 
 			            	<div id="browser_compatibility" className="container-fluid">
-			            		<h1>Frequently Asked Questions</h1>
+			            		<h1 id="faq">Frequently Asked Questions</h1>
 							  	<h2>Browser Compatibility</h2>
 							  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus!</p>
 							</div>
 
 			            	<div id="further_ref" className="container-fluid">
-			            		<h1>Model Release Notes</h1>
+			            		<h1 id="model_release">Model Release Notes</h1>
 							  	<h2>Further Reading / References</h2>
 							  	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima neque pariatur perferendis sed suscipit velit vitae voluptatem. A consequuntur, deserunt eaque error nulla temporibus!</p>
 							</div>
